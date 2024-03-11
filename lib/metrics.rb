e58a136bd8878a3c8af356a1734310804a1841b3
Carl Hörberg <carl@84codes.com>
2024-03-11 12:52:06 +0100

    acsource phase metrics

--- lib/metrics.rb
+++ lib/metrics.rb
@@ -43,33 +43,51 @@ class PrometheusMetrics
       # TYPE acload_l1_day_consumed_energy counter
       acload_l1_day_consumed_energy #{next3.acload.day_consumed_energy(1)} #{unix_ms}
       # TYPE acload_l2_current gauge
-      acload_l2_current #{next3.acload.current(1)} #{unix_ms}
+      acload_l2_current #{next3.acload.current(2)} #{unix_ms}
       # TYPE acload_l2_voltage gauge
-      acload_l2_current #{next3.acload.voltage(1)} #{unix_ms}
+      acload_l2_current #{next3.acload.voltage(2)} #{unix_ms}
       # TYPE acload_l2_active_power gauge
-      acload_l2_active_power #{next3.acload.active_power(1)} #{unix_ms}
+      acload_l2_active_power #{next3.acload.active_power(2)} #{unix_ms}
       # TYPE acload_l2_reactive_power gauge
-      acload_l2_reactive_power #{next3.acload.reactive_power(1)} #{unix_ms}
+      acload_l2_reactive_power #{next3.acload.reactive_power(2)} #{unix_ms}
       # TYPE acload_l2_apparent_power gauge
-      acload_l2_apparent_power #{next3.acload.apparent_power(1)} #{unix_ms}
+      acload_l2_apparent_power #{next3.acload.apparent_power(2)} #{unix_ms}
       # TYPE acload_l2_power_factor gauge
-      acload_l2_power_factor #{next3.acload.power_factor(1)} #{unix_ms}
+      acload_l2_power_factor #{next3.acload.power_factor(2)} #{unix_ms}
       # TYPE acload_l2_day_consumed_energy counter
-      acload_l2_day_consumed_energy #{next3.acload.day_consumed_energy(1)} #{unix_ms}
+      acload_l2_day_consumed_energy #{next3.acload.day_consumed_energy(2)} #{unix_ms}
       # TYPE acload_l3_current gauge
-      acload_l3_current #{next3.acload.current(1)} #{unix_ms}
+      acload_l3_current #{next3.acload.current(3)} #{unix_ms}
       # TYPE acload_l3_voltage gauge
-      acload_l3_current #{next3.acload.voltage(1)} #{unix_ms}
+      acload_l3_current #{next3.acload.voltage(3)} #{unix_ms}
       # TYPE acload_l3_active_power gauge
-      acload_l3_active_power #{next3.acload.active_power(1)} #{unix_ms}
+      acload_l3_active_power #{next3.acload.active_power(3)} #{unix_ms}
       # TYPE acload_l3_reactive_power gauge
-      acload_l3_reactive_power #{next3.acload.reactive_power(1)} #{unix_ms}
+      acload_l3_reactive_power #{next3.acload.reactive_power(3)} #{unix_ms}
       # TYPE acload_l3_apparent_power gauge
-      acload_l3_apparent_power #{next3.acload.apparent_power(1)} #{unix_ms}
+      acload_l3_apparent_power #{next3.acload.apparent_power(3)} #{unix_ms}
       # TYPE acload_l3_power_factor gauge
-      acload_l3_power_factor #{next3.acload.power_factor(1)} #{unix_ms}
+      acload_l3_power_factor #{next3.acload.power_factor(3)} #{unix_ms}
       # TYPE acload_l3_day_consumed_energy counter
       acload_l3_day_consumed_energy #{next3.acload.day_consumed_energy(1)} #{unix_ms}
+      # TYPE acsource_l1_voltage gauge
+      acsource_l1_voltage #{next3.acsource.voltage(1)} #{unix_ms}
+      # TYPE acsource_l1_current gauge
+      acsource_l1_current #{next3.acsource.current(1)} #{unix_ms}
+      # TYPE acsource_l1_power_factor gauge
+      acsource_l1_power_factor #{next3.acsource.power_factor(1)} #{unix_ms}
+      # TYPE acsource_l2_voltage gauge
+      acsource_l2_voltage #{next3.acsource.voltage(2)} #{unix_ms}
+      # TYPE acsource_l2_current gauge
+      acsource_l2_current #{next3.acsource.current(2)} #{unix_ms}
+      # TYPE acsource_l2_power_factor gauge
+      acsource_l2_power_factor #{next3.acsource.power_factor(2)} #{unix_ms}
+      # TYPE acsource_l3_voltage gauge
+      acsource_l3_voltage #{next3.acsource.voltage(3)} #{unix_ms}
+      # TYPE acsource_l3_current gauge
+      acsource_l3_current #{next3.acsource.current(3)} #{unix_ms}
+      # TYPE acsource_l3_power_factor gauge
+      acsource_l3_power_factor #{next3.acsource.power_factor(3)} #{unix_ms}
       EOF
     end
 
