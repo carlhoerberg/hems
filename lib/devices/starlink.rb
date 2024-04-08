878c330e4c3a8422e7728b5dbf9db7fc07dd4dfc
Carl Hörberg <carl@84codes.com>
2024-04-08 23:05:04 +0200

    raise starlink error, not runtime error

--- lib/devices/starlink.rb
+++ lib/devices/starlink.rb
@@ -4,9 +4,11 @@ class Devices
   class Starlink
     def status
       json = `grpcurl -plaintext -d '{"get_status": {}}' 192.168.100.1:9200 SpaceX.API.Device.Device/Handle`
-      raise "Starlink status failure: #{json}" unless $?.success?
+      raise Error.new("Status failure: #{json}") unless $?.success?
 
       JSON.parse(json)
     end
+
+    class Error < StandardError; end
   end
 end
