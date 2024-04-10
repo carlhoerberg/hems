98cf64f43268e08a034cabe90aba8858b3131ac1
Carl Hörberg <carl@84codes.com>
2024-04-10 11:22:43 +0200

    new ip to relay

--- lib/devices/relays.rb
+++ lib/devices/relays.rb
@@ -2,7 +2,7 @@ require_relative "../modbus/tcp"
 
 module Devices
   class Relays
-    def initialize(host = "192.168.1.200", port = 502)
+    def initialize(host = "192.168.0.200", port = 502)
       @modbus = Modbus::TCP.new(host, port).unit(1)
     end
 
