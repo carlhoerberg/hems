942bc8dfeafbc8c6c6a0b937df1ad00716aa7937
Carl Hörberg <carl@84codes.com>
2024-04-24 22:41:51 +0200

    no read_timeout for udp sockets

--- lib/devices/shelly.rb
+++ lib/devices/shelly.rb
@@ -42,7 +42,7 @@ class Devices
   end
 
   class ShellyUDP
-    @@udp = UDPSocket.new.tap { |u| u.read_timeout = 1 }
+    @@udp = UDPSocket.new
     @@lock = Mutex.new
 
     def initialize(host, port)
