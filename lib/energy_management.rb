ea8d47908ca3bdc8ed0c5de6491c9eaee3e1ec30
Carl Hörberg <carl@84codes.com>
2024-04-27 10:43:33 +0200

    energy management with load shedding

--- lib/energy_management.rb
+++ lib/energy_management.rb
@@ -9,20 +9,8 @@ class EnergyManagement
     until @stopped
       soc = @devices.next3.battery.soc
       puts "SOC: #{soc}%"
-      if soc <= 10
-        start_genset
-        #puts "Genset is running, wait for ready to load"
-        #sleep 1 until @devices.genset.ready_to_load?
-        #puts "Genset is ready to load"
-        #puts "Enabling charging"
-        #next3.acsource.enable
-      elsif soc >= 90
-        #puts "Disable charging"
-        #next3.acsource.disable
-        #puts "Letting genset cool down for 60s"
-        #sleep 60 # let genset cool down
-        stop_genset
-      end
+      genset_support(soc)
+      load_shedding(soc)
       sleep 5
     end
   end
@@ -31,17 +19,47 @@ class EnergyManagement
     @stopped = true
   end
 
+  # Turn on the 9kw heater when solar is power limited
+  # Turn off if load > 16kw
+  # Turn off when SOC < 85
+  # Turn off if forecast doesn't expect us to reach 100% SOC at the end of the solar day
+  def load_shedding(soc = @devices.next3.battery.soc)
+    if @devices.relays.heater_9kw?
+      if soc < 85
+        puts "SOC #{soc}%, turning off 9kw heater"
+        @devices.relays.heater_9kw = false
+      elsif (tp = @devices.next3.acloads.total_apparent_power) > 16_000
+        puts "Total power #{tp}, turning off 9kw heater"
+        @devices.relays.heater_9kw = false
+      end
+    else
+      if @devices.next3.solar.excess? &&
+          15_000 - @devices.next3.acloads.total_apparent_power > 9_000
+        puts "Solar excess, turning on 9kw heater"
+        @devices.relays.heater_9kw = true
+      end
+    end
+  end
+
+  def genset_support(soc = @devices.next3.battery.soc)
+    if soc <= 10
+      start_genset
+    elsif soc >= 80
+      stop_genset
+    end
+  end
+
   def start_genset
     @devices.relay.open_air_vents
-    return if @devices.genset.status[:running]
+    return if @devices.genset.is_running?
     puts "Opening air vents, takes 2:30"
     sleep 150 # it takes 2:30 for the vents to fully open
     puts "Air vents should be fully open"
 
     puts "Starting genset"
     @devices.genset.start
-    sleep 2 # should have started in this time
-    unless @devices.genset.status[:running]
+    sleep 3 # should have started in this time
+    unless @devices.genset.is_running?
       puts "Genset didn't start", "Status: #{@devices.genset.status}"
       raise "Genset didn't start"
     end
@@ -49,41 +67,15 @@ class EnergyManagement
   end
 
   def stop_genset
+    return unless @devices.genset.is_running?
     puts "Stopping genset"
     @devices.genset.stop
-    sleep 2
-    if genset.status[:running]
+    sleep 3
+    if @devices.genset.is_running?
       puts "Genset did not stop", "Status: #{genset.status}"
       raise "Genset didn't stop"
     end
     puts "Closing air vents"
     @devices.relay.close_air_vents
-  end
-
-  def charge_from_genset
-    current = 16
-    acsource = @devices.next3.acsource
-    acsource.rated_current = current
-    acsource.enable
-    loop do
-      case genset.frequency
-      when 0
-        puts "Genset has failed", "Status: #{genset.status}"
-        raise "Genset has failed"
-      when ..49.2
-        current -= 1
-        acsource.rated_current = current
-        puts "AcSourceCurrent=#{current}"
-      when 49.2..49.5
-        # hold steady
-      when 49.5..
-        if @devices.next3.battery.charging_current_high_limit > current
-          current += 1
-          acsource.rated_current = current
-          puts "AcSourceCurrent=#{current}"
-        end
-      end
-      sleep 0.5
-    end
   end
 end
