73c7d9207120cb0356a6ded1e30a3f67188df140
Carl Hörberg <carl@84codes.com>
2024-04-27 10:48:11 +0200

    device relays

--- lib/devices.rb
+++ lib/devices.rb
@@ -3,9 +3,10 @@ require_relative "./devices/genset"
 require_relative "./devices/eta"
 require_relative "./devices/starlink"
 require_relative "./devices/shelly"
+require_relative "./devices/relays"
 
 class Devices
-  attr_reader :next3, :genset, :eta, :starlink, :shelly
+  attr_reader :next3, :genset, :eta, :starlink, :shelly, :relays
 
   def initialize
     @next3 = Next3.new
@@ -13,5 +14,6 @@ class Devices
     @eta = ETA.new("192.168.0.11")
     @starlink = Starlink.new
     @shelly = Shelly.new
+    @relays = Relays.new
   end
 end
