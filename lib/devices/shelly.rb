dbec6615eea2d51915f823a93dd0b3c161c6aa1a
Carl Hörberg <carl@84codes.com>
2024-10-21 11:53:48 +0200

    p -> puts

--- lib/devices/shelly.rb
+++ lib/devices/shelly.rb
@@ -64,7 +64,8 @@ class Devices
       @@lock.synchronize do
         @@udp.send(request.to_json, 0, @host, @port)
         begin
-          p bytes, _from = @@udp.recvfrom_nonblock(4096)
+          bytes, _from = @@udp.recvfrom_nonblock(4096)
+          puts "UDP RPC response: #{bytes}"
           response = JSON.parse(bytes)
           raise Error.new("Invalid ID in response") if response["id"] != request[:id]
           raise Error.new(resp.dig("error", "message")) if response["error"]
