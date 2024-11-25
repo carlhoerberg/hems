d285a83776e3a1ba074f2213051fea174bc32df6
Carl Hörberg <carl@84codes.com>
2024-11-25 11:59:55 +0100

    weco

--- lib/devices/weco.rb
+++ lib/devices/weco.rb
@@ -5,13 +5,13 @@ class Devices
     using Modbus::TypeExtensions
 
     def initialize
-      @weco = Modbus::RTU.new("/dev/ttyUSB?", 115200).unit(1)
+      @weco = Modbus::RTU.new("/dev/ttyUSB?", 115200)
     end
 
     def any
       @weco.serial.write "\x00\x03\x00\x00\x00\x01"
-      p @weco.read_holding_registers(1, 38)
-      p @weco.read_holding_registers(39, 6)
+      p @weco.unit(1).read_holding_registers(1, 38)
+      p @weco.unit(1).read_holding_registers(39, 6)
     end
   end
 end
