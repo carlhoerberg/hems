f89d97ba65184c4a353127e054d5974154649ba4
Carl Hörberg <carl@84codes.com>
2024-12-11 11:44:47 +0100

    starlink metrics with history

--- lib/metrics.rb
+++ lib/metrics.rb
@@ -99,7 +99,7 @@ class PrometheusMetrics
       threads = [
         Thread.new { @@next3.result_with_hash({ t:, next3: @devices.next3 }) },
         Thread.new { @@eta.result_with_hash({ t:, eta: @devices.eta }) },
-        Thread.new { @@starlink.result_with_hash({ t:, status: @devices.starlink.status }) },
+        Thread.new { @@starlink.result_with_hash({ t:, metrics: @devices.starlink.metrics }) },
         Thread.new { @@shelly.result_with_hash({ t:, devices: @devices.shelly.devices }) },
         Thread.new { @@ups.result_with_hash({ t:, ups: @devices.ups }) },
         Thread.new { @@unifi.result_with_hash({ t:, unifi_health: @devices.unifi.health }) },
