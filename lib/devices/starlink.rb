271de478e7df62517c3d48e9afd112f8984ab16d
Carl Hörberg <carl@84codes.com>
2024-12-11 11:44:47 +0100

    starlink metrics with history

--- lib/devices/starlink.rb
+++ lib/devices/starlink.rb
@@ -9,6 +9,33 @@ class Devices
       JSON.parse(json)
     end
 
+    # History is stored in a ring buffer, per second
+    # dishGetHistory:
+    # current: take the length of the metric array modulus current to get the latest metric
+    # powerIn, uplinkThroughputBps, downlinkThroughputBps, popPingLatencyMs, popPingDrotRate
+    # outages is an array of objects with recent outages
+    def history
+      json = `grpcurl -plaintext -d '{"get_history": {}}' 192.168.100.1:9200 SpaceX.API.Device.Device/Handle`
+      raise Error.new("Status failure: #{json}") unless $?.success?
+
+      JSON.parse(json)
+    end
+
+    # Metrics for the last 5 seconds
+    def metrics
+      h = history["dishGetHistory"]
+      current = h["current"].to_i
+      %w(powerIn uplinkThroughputBps downlinkThroughputBps popPingLatencyMs popPingDropRate).to_h do |key|
+        values = h[key]
+        index = current % values.length # index of the last updated value in the ring buffer
+        ts = Time.now.to_f.round * 1000 # round to full second
+        value = Array.new([5, values.length].min) do |i|
+          [values[index - i], ts - i]
+        end
+        [key.gsub(/([A-Z])/, '_\1').downcase, value]
+      end
+    end
+
     class Error < StandardError; end
   end
 end
