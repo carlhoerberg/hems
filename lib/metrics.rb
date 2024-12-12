0e624df10ba9e8cbd44320d9693453068ffd2248
Carl Hörberg <carl@84codes.com>
2024-12-12 10:23:32 +0100

    1s timeout on modbus tcp reads

--- lib/metrics.rb
+++ lib/metrics.rb
@@ -103,7 +103,7 @@ class PrometheusMetrics
         Thread.new { @@shelly.result_with_hash({ t:, devices: @devices.shelly.devices }) },
         Thread.new { @@ups.result_with_hash({ t:, ups: @devices.ups }) },
         Thread.new { @@unifi.result_with_hash({ t:, unifi_health: @devices.unifi.health }) },
-        #Thread.new { @@topas.result_with_hash({ t:, measurements: @devices.topas.measurements, status: @devices.topas.status }) },
+        Thread.new { @@topas.result_with_hash({ t:, measurements: @devices.topas.measurements, status: @devices.topas.status }) },
         Thread.new do
           @@genset.result_with_hash({ t:, measurements: @devices.genset.measurements })
         rescue EOFError
