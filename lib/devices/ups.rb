3175f1ba761c6702fccdbb07b521826d281eb73e
Carl Hörberg <carl@84codes.com>
2024-12-23 00:03:22 +0100

    new ups metrics

--- lib/devices/ups.rb
+++ lib/devices/ups.rb
@@ -8,32 +8,52 @@ class Devices
       @modbus = Modbus::TCP.new(host, port).unit(1)
     end
 
+    def status
+      @modbus.read_holding_registers(0, 2)
+    end
+
     def soc
       (@modbus.read_holding_register(130) / 2.0**9).round(1)
     end
 
+    def battery_voltage
+      (@modbus.read_holding_register(131) / 32.0).round(2)
+    end
+
     def temperature
-      (@modbus.read_holding_register(135) / 2.0**7).round(1)
+      (@modbus.read_holding_register(135) / 2.0**7).round(1).to_i16
+    end
+
+    def active_power
+      pct = @modbus.read_holding_register(136) / 256.0 / 100.0
+      rated = @modbus.read_holding_register(589)
+      (pct * rated).round(2)
+    end
+
+    def apparent_power
+      pct = @modbus.read_holding_register(138) / 256.0 / 100.0
+      rated = @modbus.read_holding_register(588)
+      (pct * rated).round(2)
     end
 
     def output_current
-      (@modbus.read_holding_register(140) / 2.0**5).round(1)
+      (@modbus.read_holding_register(140) / 2.0**5).round(2)
     end
 
     def output_voltage
-      (@modbus.read_holding_register(142) / 2.0**6).round(1)
+      (@modbus.read_holding_register(142) / 2.0**6).round(2)
     end
 
     def frequency
-      (@modbus.read_holding_register(144) / 2.0**7).round(1)
+      (@modbus.read_holding_register(144) / 2.0**7).round(2)
     end
 
     def consumed_watt_hours
-      @modbus.read_holding_registers(145, 2).to_i32
+      @modbus.read_holding_registers(145, 2).to_u32
     end
 
     def input_voltage
-      (@modbus.read_holding_register(151) / 2.0**6).round(1)
+      (@modbus.read_holding_register(151) / 2.0**6).round(2)
     end
 
     def runtime_remaining
