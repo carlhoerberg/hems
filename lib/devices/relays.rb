fdab4194afa2cfd2430c2a9e5c68c47c4f9d9750
Carl Hörberg <carl@84codes.com>
2025-01-10 16:26:39 +0100

    reverse air vents

--- lib/devices/relays.rb
+++ lib/devices/relays.rb
@@ -54,15 +54,15 @@ class Devices
 
     def open_air_vents
       return if @air_vents_open
-      activate(3)
-      activate(4)
+      deactivate(3)
+      deactivate(4)
       @air_vents_open = true
     end
 
     def close_air_vents
       return unless @air_vents_open
-      deactivate(3)
-      deactivate(4)
+      activate(3)
+      activate(4)
       @air_vents_open = false
     end
   end
