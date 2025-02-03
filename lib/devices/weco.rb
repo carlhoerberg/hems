c1ebf653f3eae7e1f085f0cd6bb6da33e3063425
Carl Hörberg <carl@84codes.com>
2025-02-03 15:25:55 +0100

    weco in devices

--- lib/devices/weco.rb
+++ lib/devices/weco.rb
@@ -7,26 +7,24 @@ class Devices
       @serial = UART.open(device, 115200)
     end
 
-    def any
+    def status
+      packs = Array.new(6)
       @serial.write "\x01\x03\x00\x01\x00\x26\x1c\x2a"
-      p main_pack_response
-      #@serial.write "\x01\x03\x00\x27\x00\x06\x1c\x38"
-      #response
-      #@serial.write "\x01\x03\x00\x2e\x00\x03\x1c\x38"
-      #response
-      #@serial.write "\x01\x03\x00\x52\x00\x07\x1c\x22"
-      #response
+      packs << main_pack_response
       @serial.write "\x01\x03\x00\x79\x00\x0a\x1c\x6a" # module 1
-      p battery_pack_response
+      packs << battery_pack_response
       @serial.write "\x01\x03\x00\x83\x00\x0a\x1c\x5a" # module 2
-      p battery_pack_response
+      packs << battery_pack_response
       @serial.write "\x01\x03\x00\x8d\x00\x0a\x1c\x18"
-      p battery_pack_response
+      packs << battery_pack_response
       @serial.write "\x01\x03\x00\x97\x00\x0a\x1c\x5a"
-      p battery_pack_response
+      packs << battery_pack_response
       @serial.write "\x01\x03\x00\xa1\x00\x0a\x1c\x50"
-      p battery_pack_response
+      packs << battery_pack_response
+      packs
     end
+
+    private
 
     def main_pack_response
       _unit, _function, len = @serial.read(3).unpack("CCC")
