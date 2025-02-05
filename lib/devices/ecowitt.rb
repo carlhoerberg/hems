d0cc3aa05611d90879356662de74d3c3b917a38a
Carl Hörberg <carl@84codes.com>
2025-02-05 10:19:07 +0100

    round ecowitt measurements

--- lib/devices/ecowitt.rb
+++ lib/devices/ecowitt.rb
@@ -17,14 +17,14 @@ class Devices
       values = @m.read_holding_registers(0x0165, 9)
       {
         light: values[0] * 10,
-        uvi: values[1] * 0.1,
-        temperature: (values[2] - 400) * 0.1,
+        uvi: (values[1] * 0.1).round(1),
+        temperature: ((values[2] - 400) * 0.1).round(1),
         humidity: values[3],
-        wind_speed: values[4] * 0.1,
-        gust_wpeed: values[5] * 0.1,
+        wind_speed: (values[4] * 0.1).round(1),
+        gust_wpeed: (values[5] * 0.1).round(1),
         wind_direction: values[6],
-        rainfall: values[7] * 0.1,
-        abs_pressure: values[8] * 0.1,
+        rainfall: (values[7] * 0.1).round(1),
+        abs_pressure: (values[8] * 0.1).round(1),
       }
     end
   end
