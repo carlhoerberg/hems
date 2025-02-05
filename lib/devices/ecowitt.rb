d10e1292cc09b4d1432d8d359417427397589fe6
Carl Hörberg <carl@84codes.com>
2025-02-05 10:25:33 +0100

    ecowit typo

--- lib/devices/ecowitt.rb
+++ lib/devices/ecowitt.rb
@@ -8,11 +8,6 @@ class Devices
       @m = Modbus::RTU.new("/dev/ttyUSB1").unit(0x90)
     end
 
-    def temperature
-      v = @m.read_holding_register(0x0167)
-      ((v - 400) / 10.0).round(1)
-    end
-
     def measurements
       values = @m.read_holding_registers(0x0165, 9)
       {
@@ -21,7 +16,7 @@ class Devices
         temperature: ((values[2] - 400) * 0.1).round(1),
         humidity: values[3],
         wind_speed: (values[4] * 0.1).round(1),
-        gust_wpeed: (values[5] * 0.1).round(1),
+        gust_speed: (values[5] * 0.1).round(1),
         wind_direction: values[6],
         rainfall: (values[7] * 0.1).round(1),
         abs_pressure: (values[8] * 0.1).round(1),
