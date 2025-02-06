dcca96c284fd298c86e6a2a98232b9b1d3004168
Carl Hörberg <carl@84codes.com>
2025-02-06 04:00:22 +0100

    weco warns

--- lib/devices/weco.rb
+++ lib/devices/weco.rb
@@ -104,40 +104,51 @@ class Devices
     end
 
     def warn_response
-      @serial.write "\x01\x03\x00\x33\x00\x1d\x1c\x32"
-
-      _unit, _function, len = @serial.read(3).unpack("CCC")
-      values = @serial.read(len).unpack("s>*")
-      _crc1, _crc2 = @serial.read(2).unpack("CC")
+      # "\x01\x03\x00\x33\x00\x1d\x1c\x32"
+      values = read_holding_registers(0x33, 0x1d)
       {
-        #Warn_Over_Vol_Set_2 = Convert.ToInt16((int) Data[3] * 256 + (int) Data[4]);
-        #Warn_Over_Vol_Set_1 = Convert.ToInt16((int) Data[5] * 256 + (int) Data[6]);
-        #Warn_Low_Vol_Set_2 = Convert.ToInt16((int) Data[7] * 256 + (int) Data[8]);
-        #Warn_Low_Vol_Set_1 = Convert.ToInt16((int) Data[9] * 256 + (int) Data[10]);
-        #Warn_Over_SumVol_Set = (int) Data[11] * 256 + (int) Data[12];
-        #Warn_Over_SumVol_Rec = (int) Data[13] * 256 + (int) Data[14];
-        #Warn_Low_SumVol_Set = (int) Data[15] * 256 + (int) Data[16];
-        #Warn_Low_SumVol_Rec = (int) Data[17] * 256 + (int) Data[18];
-        #Warn_Over_DisChargeCur_Set = (short) ((int) Data[19] * 256 + (int) Data[20]);
-        #Warn_Over_ChargeCur_Set = (short) ((int) Data[21] * 256 + (int) Data[22]);
-        #Warn_Over_Tmp_Set_Charge_2 = (short) ((int) Data[23] * 256 + (int) Data[24]);
-        #Warn_Over_Tmp_Set_Charge_1 = (short) ((int) Data[25] * 256 + (int) Data[26]);
-        #Warn_Over_Tmp_Set_Discharge_2 = (short) ((int) Data[27] * 256 + (int) Data[28]);
-        #Warn_Over_Tmp_Set_Discharge_1 = (short) ((int) Data[29] * 256 + (int) Data[30]);
-        #Warn_Low_Tmp_Set_Charge_2 = (short) ((int) Data[31] * 256 + (int) Data[32]);
-        #Warn_Low_Tmp_Set_Charge_1 = (short) ((int) Data[33] * 256 + (int) Data[34]);
-        #Warn_Low_Tmp_Set_Discharge_2 = (short) ((int) Data[35] * 256 + (int) Data[36]);
-        #Warn_Low_Tmp_Set_Discharge_1 = (short) ((int) Data[37] * 256 + (int) Data[38]);
-        #VolTmp.Rec_Charge_Vol = Convert.ToInt32((int) Data[39] * 256 + (int) Data[40]);
-        #VolTmp.Rec_Charge_Cur = Convert.ToDouble((int) Data[41] * 256 + (int) Data[42]);
-
-        #VolTmp.Hard_Sum_Vol = Convert.ToInt32((int) Data[45] * 256 + (int) Data[46]);
-        #VolTmp.Hall_Cur = (short) ((int) Data[47] * 256 + (int) Data[48]);
-        #VolTmp.Force_charge = (int) (short) ((int) Data[49] * 256 + (int) Data[50]);
-        #VolTmp.Ch_Oc_times = (int) (short) ((int) Data[51] * 256 + (int) Data[52]);
-        #VolTmp.Disch_Oc_times = (int) (short) ((int) Data[53] * 256 + (int) Data[54]);
-        #VolTmp.Disch_limit_Vol = (double) (short) ((int) Data[55] * 256 + (int) Data[56]);
-        #VolTmp.Disch_limit_Cur = (double) (short) ((int) Data[57] * 256 + (int) Data[58]);
+        Warn_Over_Vol_Set_2: values[0],
+        Warn_Over_Vol_Set_1: values[1],
+        Warn_Low_Vol_Set_2: values[2],
+        Warn_Low_Vol_Set_1: values[3],
+        Warn_Over_SumVol_Set: values[4],
+        Warn_Over_SumVol_Rec: values[5],
+        Warn_Low_SumVol_Set: values[6],
+        Warn_Low_SumVol_Rec: values[7],
+        Warn_Over_DisChargeCur_Set: values[7],
+        Warn_Over_ChargeCur_Set: values[8],
+        Warn_Over_Tmp_Set_Charge_2: values[9],
+        Warn_Over_Tmp_Set_Charge_1: values[10],
+        Warn_Over_Tmp_Set_Discharge_2: values[11],
+        Warn_Over_Tmp_Set_Discharge_1: values[12],
+        Warn_Low_Tmp_Set_Charge_2: values[13],
+        Warn_Low_Tmp_Set_Charge_1: values[14],
+        Warn_Low_Tmp_Set_Discharge_2: values[15],
+        Warn_Low_Tmp_Set_Discharge_1: values[16],
+        VolTmp_Rec_Charge_Vol: values[17],
+        VolTmp_Rec_Charge_Cur: values[18],
+        # values[19] <= Data[43, 44]
+        #Warn.DI1 = ((int) Data[44] & 1) != 1 ? (byte) 0 : (byte) 1;
+        #Warn.DI2 = ((int) Data[44] & 2) != 2 ? (byte) 0 : (byte) 1;
+        #Warn.DO1 = ((int) Data[44] & 4) != 4 ? (byte) 0 : (byte) 1;
+        #Warn.DO2 = ((int) Data[44] & 8) != 8 ? (byte) 0 : (byte) 1;
+        #Warn.Contact_Charge = ((int) Data[44] & 16) != 16 ? (byte) 0 : (byte) 1;
+        #Warn.Contact_Ready_Charge = ((int) Data[44] & 32) != 32 ? (byte) 0 : (byte) 1;
+        #Warn.Contact_Discharge = ((int) Data[44] & 64) != 64 ? (byte) 0 : (byte) 1;
+        #Warn.Contact_Mag_Discharge = ((int) Data[44] & 128) != 128 ? (byte) 0 : (byte) 1;
+        #if (((int) Data[43] & 1) == 1)
+        #  {
+        #    this.AGVData.Warn.Contact_Mag_Charge = (byte) 1;
+        #    break;
+        #  }
+        #  this.AGVData.Warn.Contact_Mag_Charge = (byte) 0;
+        VolTmp_Hard_Sum_Vol: values[20],
+        VolTmp_Hall_Cur: values[21],
+        VolTmp_Force_charge: values[22],
+        VolTmp_Ch_Oc_times: values[23],
+        VolTmp_Disch_Oc_times: values[24],
+        VolTmp_Disch_limit_Vol: values[25],
+        VolTmp_Disch_limit_Cur: values[26],
         #Warn.slave1_commLost = ((int) Data[60] & 1) != 1 ? (byte) 1 : (byte) 0;
         #Warn.slave2_commLost = ((int) Data[60] & 2) != 2 ? (byte) 1 : (byte) 0;
         #Warn.slave3_commLost = ((int) Data[60] & 4) != 4 ? (byte) 1 : (byte) 0;
@@ -154,22 +165,7 @@ class Devices
         #Warn.slave13_commLost = ((int) Data[59] & 16) != 16 ? (byte) 1 : (byte) 0;
         #Warn.slave14_commLost = ((int) Data[59] & 32) != 32 ? (byte) 1 : (byte) 0;
         #Warn.pack_vol_imbalance = ((int) Data[59] & 128) != 128 ? (byte) 0 : (byte) 1;
-        #Warn.DI1 = ((int) Data[44] & 1) != 1 ? (byte) 0 : (byte) 1;
-        #Warn.DI2 = ((int) Data[44] & 2) != 2 ? (byte) 0 : (byte) 1;
-        #Warn.DO1 = ((int) Data[44] & 4) != 4 ? (byte) 0 : (byte) 1;
-        #Warn.DO2 = ((int) Data[44] & 8) != 8 ? (byte) 0 : (byte) 1;
-        #Warn.Contact_Charge = ((int) Data[44] & 16) != 16 ? (byte) 0 : (byte) 1;
-        #Warn.Contact_Ready_Charge = ((int) Data[44] & 32) != 32 ? (byte) 0 : (byte) 1;
-        #Warn.Contact_Discharge = ((int) Data[44] & 64) != 64 ? (byte) 0 : (byte) 1;
-        #Warn.Contact_Mag_Discharge = ((int) Data[44] & 128) != 128 ? (byte) 0 : (byte) 1;
-        #if (((int) Data[43] & 1) == 1)
-        #  {
-        #    this.AGVData.Warn.Contact_Mag_Charge = (byte) 1;
-        #    break;
-        #  }
-        #  this.AGVData.Warn.Contact_Mag_Charge = (byte) 0;
       }
     end
-
   end
 end
