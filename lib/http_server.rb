2df46c446e5f759910e8682b653fd4642c77f7e3
Carl Hörberg <carl@84codes.com>
2025-03-04 00:23:43 +0100

    energy managment control view

--- lib/http_server.rb
+++ lib/http_server.rb
@@ -4,9 +4,10 @@ require_relative "./http_server/relays"
 require_relative "./http_server/genset"
 require_relative "./http_server/button"
 require_relative "./http_server/topas"
+require_relative "./http_server/em"
 
 class HTTPServer
-  def initialize(devices)
+  def initialize(devices, em)
     @server = WEBrick::HTTPServer.new(Port: ENV.fetch("PORT", 8000).to_i,
                                       AccessLog: [])
     @server.mount "/metrics", Metrics, devices
@@ -14,6 +15,7 @@ class HTTPServer
     @server.mount "/genset", GensetControl, devices.genset
     @server.mount "/button1", ButtonControl, devices
     @server.mount "/topas", TopasControl, devices.topas
+    @server.mount "/em", EMControl, em
     @server.mount_proc("/eta") do |_req, res|
       res.content_type = "application/xml"
       res.body = devices.eta.menu
---
+++ lib/http_server/em.rb
@@ -0,0 +1,28 @@
+class HTTPServer
+  class EMControl < WEBrick::HTTPServlet::AbstractServlet
+    def initialize(server, em)
+      super(server)
+      @em = em
+    end
+
+    @@view = ERB.new(File.read(File.join(__dir__, "..", "..", "views", "em_control.erb")))
+
+    def do_GET(req, res)
+      res.content_type = "text/html"
+      res.body = @@view.result_with_hash({ em: @em })
+    end
+
+    def do_POST(req, res)
+      form = URI.decode_www_form(req.body).to_h
+      case form["action"]
+      when "genset_auto_started" then @em.genset_auto_started = true
+      when "genset_not_auto_started" then @em.genset_auto_started = false
+      when "stop_genset" then @em.stop_genset
+      when "start_genset" then @em.start_genset
+      else raise "no action selected"
+      end
+      res.status = 303
+      res["location"] = req.path
+    end
+  end
+end
