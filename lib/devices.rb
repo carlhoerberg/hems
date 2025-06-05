7e07ef66695408d2d5da3e549d6e9ea94d5ba6dc
Carl Hörberg <carl@84codes.com>
2025-06-05 14:22:32 +0200

    envistar device

--- lib/devices.rb
+++ lib/devices.rb
@@ -9,9 +9,10 @@ require_relative "./devices/unifi"
 require_relative "./devices/topas"
 require_relative "./devices/weco"
 require_relative "./devices/ecowitt"
+require_relative "./devices/envistar"
 
 class Devices
-  attr_reader :next3, :genset, :eta, :starlink, :shelly, :relays, :ups, :unifi, :topas, :weco, :ecowitt
+  attr_reader :next3, :genset, :eta, :starlink, :shelly, :relays, :ups, :unifi, :topas, :weco, :ecowitt, :envistar
 
   def initialize
     @next3 = Next3.new
@@ -25,5 +26,6 @@ class Devices
     @topas = Topas.new
     @weco = Weco.new
     @ecowitt = Ecowitt.new
+    @envistar = Envistar.new
   end
 end
