6da7912354f824c3a06abb00d0574b36402bdcb5
Carl Hörberg <carl@84codes.com>
2025-09-19 14:32:30 +0200

    bms/current operation mode

--- shelly/shelly1minig4-a085e3bc3340/1_Envistar.js
+++ shelly/shelly1minig4-a085e3bc3340/1_Envistar.js
@@ -1,1 +1,26 @@
- 
+const operatingMode = Virtual.getHandle("enum:200");
+
+function updateOperatingMode(result, error_code, error_message) {
+  if (error_code === 0 && result && result.body) {
+    try {
+      let data = JSON.parse(result.body);
+      print("Received data:", data);
+      if (data.operating_mode !== undefined) {
+        operatingMode.setValue(JSON.stringify(data.operating_mode));
+      }
+    } catch (e) {
+      print("Error parsing JSON:", e);
+    }
+  } else {
+    print("HTTP request failed:", error_code, error_message);
+  }
+}
+
+function pollEnvistarStatus() {
+  Shelly.call("HTTP.GET", {
+    url: "http://192.168.0.2:8000/envistar/status"
+  }, updateOperatingMode);
+}
+
+Timer.set(5000, true, pollEnvistarStatus);
+pollEnvistarStatus();
