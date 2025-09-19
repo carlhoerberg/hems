d163f2721b91a1dd214e8ef950c909b0dc5c3403
Carl Hörberg <carl@84codes.com>
2025-09-19 15:42:27 +0200

    envistar on change

--- shelly/shelly1minig4-a085e3bc3340/1_Envistar.js
+++ shelly/shelly1minig4-a085e3bc3340/1_Envistar.js
@@ -21,5 +21,19 @@ function pollEnvistarStatus() {
   }, updateOperatingMode);
 }
 
+function onOperatingModeChange(ev) {
+  print("Operating mode changed to:", ev.value);
+  Shelly.call("HTTP.GET", {
+    url: "http://192.168.0.2:8000/envistar/set?operating_mode=" + ev.value,
+  }, function(result, error_code, error_message) {
+    if (error_code === 0) {
+      print("Successfully set operating mode to:", ev.value);
+    } else {
+      print("Failed to set operating mode:", error_code, error_message);
+    }
+  });
+}
+
+operatingMode.on("change", onOperatingModeChange)
 Timer.set(5000, true, pollEnvistarStatus);
 pollEnvistarStatus();
