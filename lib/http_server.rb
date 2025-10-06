d3d69e7f113b385c89c23e16af2a7ebb3d784321
Carl Hörberg <carl@84codes.com>
2025-10-06 22:33:44 +0200

    keepalive support by codex

--- lib/http_server.rb
+++ lib/http_server.rb
@@ -59,77 +59,103 @@ class HTTPServer
   private
 
   def handle_client(socket)
-    request_line = socket.gets("\r\n", chomp: true) || return # read request line
-    method, path_query, _http_version = request_line.split(" ", 3) # ["GET", "/path", "HTTP/1.1"]
-
-    # Parse path and query string
-    path, query_string = path_query.split("?", 2)
-    query = {}
-    query_string&.split("&") do |pair|
-      key, value = pair.split("=", 2)
-      query[key] = value
-    end
-
-    # Parse headers
-    headers = {}
     loop do
-      line = socket.gets("\r\n", chomp: true) || return
-      break if line.empty?
-      key, value = line.split(": ", 2)
-      headers[key.downcase] = value
-    end
-    # Read body if Content-Length is present
-    body = if headers["content-length"]
-             socket.read(headers["content-length"].to_i)
-           else
-              ""
-           end
+      request_line = socket.gets("\r\n", chomp: true)
+      break if request_line.nil?
+      next if request_line.empty?
 
-    # Handle the request
-    request = Request.new(method, path.chomp("/"), query, headers, body)
-    response = Response.new
-    first_part = path[0, path.index("/", 1) || path.length]
-    if (controller = @controllers[first_part])
-      begin
-        case method
-        when "GET"
-          controller.do_GET(request, response)
-        when "POST"
-          controller.do_POST(request, response)
-        else
-        end
-      rescue => e
-        response.status = 500
-        response.headers = {}
-        response.content_type = "text/plain"
-        response.body = "Internal Server Error\n#{e.message}\n"
+      method, path_query, http_version = request_line.split(" ", 3) # ["GET", "/path", "HTTP/1.1"]
+      http_version ||= "HTTP/1.0"
+      status_line_version = http_version.start_with?("HTTP/") ? http_version : "HTTP/1.0"
+
+      # Parse path and query string
+      path_query ||= "/"
+      path, query_string = path_query.split("?", 2)
+      query = {}
+      query_string&.split("&") do |pair|
+        key, value = pair.split("=", 2)
+        query[key] = value
       end
-    else
-      response.status = 404
-      response.body = "Not Found\n"
-    end
 
-    # Send the response
-    socket.print "HTTP/1.0 #{response.status_header}\r\n"
-    (response.headers || {}).each do |key, value|
-      socket.print "#{key}: #{value}\r\n"
+      # Parse headers
+      headers = {}
+      loop do
+        line = socket.gets("\r\n", chomp: true)
+        return if line.nil?
+        break if line.empty?
+        key, value = line.split(": ", 2)
+        headers[key.downcase] = value
+      end
+
+      # Read body if Content-Length is present
+      body = if headers["content-length"]
+               socket.read(headers["content-length"].to_i)
+             else
+               ""
+             end
+
+      # Handle the request
+      request = Request.new(method, path.chomp("/"), query, headers, body, status_line_version)
+      response = Response.new
+      first_part = path[0, path.index("/", 1) || path.length]
+      if (controller = @controllers[first_part])
+        begin
+          case method
+          when "GET"
+            controller.do_GET(request, response)
+          when "POST"
+            controller.do_POST(request, response)
+          else
+          end
+        rescue => e
+          response.status = 500
+          response.headers = {}
+          response.content_type = "text/plain"
+          response.body = "Internal Server Error\n#{e.message}\n"
+        end
+      else
+        response.status = 404
+        response.body = "Not Found\n"
+      end
+
+      connection_header = headers["connection"]&.downcase
+      keep_alive = case status_line_version
+                   when "HTTP/1.1"
+                     connection_header != "close"
+                   when "HTTP/1.0"
+                     connection_header == "keep-alive"
+                   else
+                     false
+                   end
+
+      response.headers ||= {}
+      response.headers["Connection"] ||= keep_alive ? "keep-alive" : "close"
+
+      response_body = response.body || ""
+      socket.print "#{status_line_version} #{response.status_header}\r\n"
+      response.headers.each do |key, value|
+        socket.print "#{key}: #{value}\r\n"
+      end
+      socket.print "Content-Length: #{response_body.bytesize}\r\n"
+      socket.print "\r\n"
+      socket.print response_body
+
+      break unless keep_alive
     end
-    socket.print "Content-Length: #{response.body.bytesize}\r\n"
-    socket.print "\r\n"
-    socket.print response.body
   ensure
     socket.close
   end
 
   class Request
-    attr_reader :method, :path, :query, :headers, :body
+    attr_reader :method, :path, :query, :headers, :body, :http_version
 
-    def initialize(method, path, query, headers, body)
+    def initialize(method, path, query, headers, body, http_version)
       @method = method
       @path = path
       @query = query
       @headers = headers
       @body = body
+      @http_version = http_version
     end
   end
 
