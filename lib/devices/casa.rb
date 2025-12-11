bb12e5a4764b7664d01219992601084db9c1b6d4
Carl Hörberg <carl@84codes.com>
2025-12-11 23:10:10 +0100

    Pack unsigned 16-bit integers as signed 16-bit integers

--- lib/devices/casa.rb
+++ lib/devices/casa.rb
@@ -15,7 +15,7 @@ class Devices
     end
 
     def temperatures
-      v = @modbus.read_input_registers(6200, 6).map! { |v| [v].to_i16 }
+      v = @modbus.read_input_registers(6200, 6).pack("n*").unpack("s>*")
       {
         fresh: v[0] / 10.0,
         supply_before_reheater: v[1] / 10.0,
@@ -98,7 +98,7 @@ class Devices
     end
 
     def measurements
-      v = @modbus.read_input_registers(6200, 21).map! { |v| [v].to_i16 }
+      v = @modbus.read_input_registers(6200, 21).pack("n*").unpack("s>*")
       {
         fresh_air_temperature: v[0] / 10.0,
         supply_air_before_heater_temperature: v[1] / 10.0,
