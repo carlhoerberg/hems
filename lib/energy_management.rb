b4e246a38568d78413b660fd54e6054e2a2ced38
Carl Hörberg <carl@84codes.com>
2026-03-04 14:59:13 +0100

    Idle genset 10 minutes after last demand

--- lib/energy_management.rb
+++ lib/energy_management.rb
@@ -29,10 +29,8 @@ class EnergyManagement
   INVERTER_CURRENT_LIMIT = 23
   GENSET_CURRENT_LIMIT = 50
 
-  BATTERY_KWH = 31.2
-
   GENSET_DEMAND_START_DELAY = 30    # seconds of unmet demand before starting genset
-  GENSET_DEMAND_STOP_DELAY = 15 * 60 # seconds after last demand before stopping genset
+  GENSET_DEMAND_STOP_DELAY = 10 * 60 # seconds after last demand before stopping genset
 
   def initialize(devices)
     @devices = devices
