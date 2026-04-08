971f05110e579d4ad0d8541cc8acd795bab0b851
Carl Hörberg <carl@84codes.com>
2026-04-08 07:01:00 +0200

    next3 normal arguments

--- lib/devices/next3.rb
+++ lib/devices/next3.rb
@@ -6,7 +6,7 @@ class Devices
 
     attr_reader :acload, :system_total, :battery, :battery2, :acsource, :solar, :solar2, :aux1, :converter, :converter2
 
-    def initialize(host: nil, port: nil)
+    def initialize(host = nil, port = nil)
       host ||= ENV.fetch("NEXT3_HOST", "studer-next")
       port ||= ENV.fetch("NEXT3_PORT", 502).to_i
       next3 = Modbus::TCP.new(host, port)
